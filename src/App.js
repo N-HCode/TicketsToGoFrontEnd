86195e5891d94983882a6e3da524c84d8e4418d6
N-HCode <N.HGCHub@gmail.com>
2020-10-03 10:55:31 -0700

    made app.js a class

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  //appConfig is used to apply configuration to the react program.
+  //This is different to state, as state constantly changes with the setState.
+  //Configurations do not change very often and something.
+  //This helps to keep some settings at the top level instead of the component level.
+  //So we can easily test changes in one location.
+  appConfig = {
+
+  }
+
+  //the state of the program. This will usually hold data that are constantly
+  //changing. We need a state to temporarily keep data that need to be referred
+  //to at a later time, but will also be changing a lot.
+  state = {
+
+  }
+
+  //this is the function section.
+
+
+  //The render is required for all react classes
+  render() {
+
+    return(
+    <div>
+      <div id="test">Hello world</div>
+      <div id="test2">World hello</div>
     </div>
-  );
+    );
+  }
+
+  //react lifecycles menthods: https://reactjs.org/docs/react-component.html
+
 }
 
 export default App;
