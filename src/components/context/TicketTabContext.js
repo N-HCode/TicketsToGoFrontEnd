6546b29e032d335bcd5ac368fd5f604d7e954592
N-HCode <N.HGCHub@gmail.com>
2020-10-26 14:32:35 -0700

    added ticket tab context

---
+++ src/components/context/TicketTabContext.js
@@ -0,0 +1,17 @@
+import React, { useState, createContext } from 'react';
+
+const TicketTabContext = createContext();
+
+const TicketTabContextProvider = (props) => {
+    const [ ticketTabState, setTicketTabState] = useState([])
+
+    return (
+        <TicketTabContext.Provider
+            value={ [ticketTabState, setTicketTabState] }
+        > 
+           {props.children}
+       </TicketTabContext.Provider>
+    )
+}
+
+export { TicketTabContext, TicketTabContextProvider };
