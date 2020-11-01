3670049c84c2f83407758331501ca2218cb2cbe1
N-HCode <N.HGCHub@gmail.com>
2020-11-01 13:31:47 -0800

    refactored setup page

--- src/components/organization/signupcomponents/AccountInfoInput.js
+++ src/components/organization/signupcomponents/AccountInfoInput.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import { createOrganization } from '../../../services/OrganizationService';
 
 
 
-function AccountInfoInput() {
+function AccountInfoInput(props) {
 
     const [ root, setRoot ] = useState({
         username: "",
@@ -24,24 +25,27 @@ function AccountInfoInput() {
         })
     }
 
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-    //     createOrganization(root.username, root.password, organization)
-    //     .then(response => console.log(response.data))
-    // }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        createOrganization(root.username, root.password, props.organization)
+        .then(response => console.log(response.data))
+    }
 
     
     return (
-        <form id="second_step_signup" > 
-            <i className="material-icons" onClick={goBack}>keyboard_backspace</i>  
-                <h1>Create Admin</h1>
-                <p>This will be your inital admin account with full access</p>
-                <hr></hr>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" onChange={onChangeRoot} required></input>
+        <form id="second_step_signup" onSubmit={onSubmit}> 
+            <div onClick={goBack}>
+                <i className="material-icons" >keyboard_backspace</i> 
+            </div>
+             
+            <h1>Create Admin</h1>
+            <p>This will be your initial admin account with full access</p>
+            <hr></hr>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" onChange={onChangeRoot} required></input>
 
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" onChange={onChangeRoot} required></input>
+            <label htmlFor="password">Password</label>
+            <input type="text" name="password" onChange={onChangeRoot} required></input>
             
             <button type="submit">Complete</button>
 
