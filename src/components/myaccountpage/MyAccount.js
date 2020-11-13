d93527d66ea07311068646e49a453a9f65b8f456
N-HCode <N.HGCHub@gmail.com>
2020-11-12 20:26:07 -0800

    improved styling

--- src/components/myaccountpage/MyAccount.js
+++ src/components/myaccountpage/MyAccount.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useRef, useReducer} from 'react'
 
 const MyAccount = () => {
+
+
+    const navbar = useRef();
+
     return (
         <div className="main_container">
             <div id="my_account_page_container">
-                <div id="my_account_background">
 
+                <div id="my_account_nav_menu_container">
+                    <ul className="my_account_nav_menu" ref={navbar}>
+                        <li><i className="material-icons">lock</i>Security</li>
+                        <li className="active_tab">Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                    </ul>
+
+                </div>
+
+                <div className="single_page_container">
 
                 </div>
 
