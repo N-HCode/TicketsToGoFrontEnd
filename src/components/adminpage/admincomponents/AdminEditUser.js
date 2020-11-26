44321d6e3f85ceac2ab3f63610d8305547d9adf4
N-HCode <N.HGCHub@gmail.com>
2020-11-25 20:45:45 -0800

    setting up modal

---
+++ src/components/adminpage/admincomponents/AdminEditUser.js
@@ -0,0 +1,14 @@
+import React, { userState,useReducer} from 'react'
+
+const AdminEditUser = () => {
+
+    const [state, setstate] = useState(initialState)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AdminEditUser
