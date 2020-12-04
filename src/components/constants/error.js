df27cf04c64306ae991231194290bc9cbf41bf8b
N-HCode <N.HGCHub@gmail.com>
2020-12-04 14:55:19 -0800

    updating edit user

--- src/components/constants/error.js
+++ src/components/constants/error.js
@@ -1,5 +1,6 @@
 export const ERROR = {
     currentPW: "Current password is incorrect",
     samePW: "New password cannot be the same as current password",
-    confirmPW: "Confirm Password and New Password do not match"
+    confirmPW: "Confirm Password and New Password do not match",
+    usernameTaken: "Please use another username"
 }
