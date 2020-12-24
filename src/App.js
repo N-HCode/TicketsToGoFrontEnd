2437d22cd526a1cdb7a1c331382b433ff37b104e
N-HCode <N.HGCHub@gmail.com>
2020-12-24 15:37:55 -0800

    updated status with create ticket

--- src/App.js
+++ src/App.js
@@ -59,7 +59,6 @@ class App extends React.Component {
   paths = {
     editUser: "/editUser",
     createTicket: "/createTicket",
-  
     login: "/login",
     createOrganization: "/createOrganization",
     userList: "/users",
