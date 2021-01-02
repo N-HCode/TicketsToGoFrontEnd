2516e4c50e3827833352cb666c76a9e76839de8d
N-HCode <N.HGCHub@gmail.com>
2021-01-02 10:32:27 -0800

    updated styling

--- src/services/StatusListService.js
+++ src/services/StatusListService.js
@@ -4,12 +4,26 @@ const getAllStatus = (statusListId) => {
     return axios.get(`http://localhost:8080/statuslist/${statusListId}`);
 }
 
+
+//Need to change the header so that it expects a String for the body and not an entity
 const addAStatus = (statusListId, status) => {
-    return axios.put(`http://localhost:8080/statuslist/add/${statusListId}`, status);
+    return axios.put(`http://localhost:8080/statuslist/add/${statusListId}`
+    , status
+    , {
+
+        headers: {"Content-Type": "text/plain"}
+    }
+    );
 }
 
 const removeAStatus = (statusListId, status) => {
-    return axios.put(`http://localhost:8080/statuslist/remove/${statusListId}`, status);
+    return axios.put(`http://localhost:8080/statuslist/remove/${statusListId}`
+    , status
+    , {
+
+        headers: {"Content-Type": "text/plain"}
+    }
+    );
 }
 
 
